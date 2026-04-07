2e59447579969dcccd0d339b0dd814b36ddbc188
Kartik Singh <kartik123@gmail.com>
2026-04-07 22:15:21 +0530

    Fixed project + cleaned git

---
+++ server/routes/interview.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import  isAuth from "../middlewares/isAuth.js";
+import {upload} from "../middlewares/multer.js";
+import { analyzeResume, finishInterview } from '../controllers/InterviewController.js';
+import   {generateQuestion} from '../controllers/InterviewController.js'
+import { submitAnswer } from '../controllers/InterviewController.js';
+const interviewRouter = express.Router();
+interviewRouter.post('/resume',isAuth,upload.single('resume'),analyzeResume)
+interviewRouter.post('/generate-questions',isAuth,generateQuestion)
+interviewRouter.post('/submit-answer',isAuth,submitAnswer)
+interviewRouter.post("/finish",isAuth,finishInterview)
+export default interviewRouter;
