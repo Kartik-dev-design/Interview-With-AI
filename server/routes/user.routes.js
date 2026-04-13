341ded382e7b0fc8f9e92e0baa6ccf1dcbbd09e2
Kartik Singh <kartik123@gmail.com>
2026-04-14 02:45:50 +0530

    removed node_modules completely

--- server/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import isAuth from "../middlewares/isAuth.js";
-import {getCurrentUser} from "../controllers/User.controller.js";
-const userrouter=express.Router();
-userrouter.get('/current-user',isAuth,getCurrentUser)
-export default userrouter;
